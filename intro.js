de82e165684dd0df5df6c522c6cdc6d1b4ef90ab
finmanfr <finsterstewart@gmail.com>
2026-01-04 21:52:01 -0500

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const video = document.getElementById("introVideo");
-  const unmuteBtn = document.getElementById("unmute-btn");
 
-  unmuteBtn.addEventListener("click", () => {
-    video.muted = false;
-    video.volume = 1;
-    video.play(); // important for Safari/iOS
-    unmuteBtn.style.display = "none";
-  });
-});
+  const video = document.getElementById("introVideo");
+
+const unmuteBtn = document.getElementById("unmute-btn");
+
+unmuteBtn.onclick = () => {
+  video.muted = false;
+  video.volume = 1;
+  video.play(); // required for Safari/mobile
+  unmuteBtn.style.display = "none";
+};
