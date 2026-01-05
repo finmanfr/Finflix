d2cdb3df36ee9bf1c7d7848710420ea684c84e70
finmanfr <finsterstewart@gmail.com>
2026-01-04 22:22:08 -0500

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,14 +1,15 @@
-const video = document.getElementById("introVideo");
 const unmuteBtn = document.getElementById("unmute-btn");
+const video = document.getElementById("introVideo");
 const intro = document.getElementById("intro-container");
 const main = document.getElementById("main-content");
 
+// End intro
 function endIntro() {
   intro.style.display = "none";
   main.style.display = "block";
 }
 
-// Unmute
+// Unmute button
 unmuteBtn.onclick = () => {
   video.muted = false;
   video.volume = 1;
@@ -16,20 +17,5 @@ unmuteBtn.onclick = () => {
   unmuteBtn.style.display = "none";
 };
 
-// When video ends → same as skip
+// When video ends
 video.onended = endIntro;
-
-<script>
-const movies = [
-  "K+K.png",
-];
-
-const row = document.getElementById("movie-row");
-
-movies.forEach(icon => {
-  const div = document.createElement("div");
-  div.className = "movie";
-  div.style.backgroundImage = `url(movies/icons/${icon})`;
-  row.appendChild(div);
-});
-</script>
